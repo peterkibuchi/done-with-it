f2abb0fd3c72241621a44f06ebc7f93bb392df8a
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-02-14 05:54:41 +0300

    Update Logger Function

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -1,8 +1,6 @@
 import Bugsnag from "@bugsnag/expo";
 
-const log = (error) => {
-  return __DEV__ ? console.log(error) : Bugsnag.notify(error);
-};
+const log = (error) => Bugsnag.notify(error);
 
 const start = () => Bugsnag.start();
 
